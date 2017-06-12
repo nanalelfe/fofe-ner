15ccd98009c30e82463e019fd78a37f48f94c549
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2017-06-12 14:53:40 -0400

    Changed parameters

--- source/gigaword2feature.pyx
+++ source/gigaword2feature.pyx
@@ -1158,8 +1158,9 @@ class batch_constructor:
             if feature_choice & 128 > 0:
                 logger.info("sentence.sentence[begin_idx:end_idx]")
                 logger.info(sentence.sentence[begin_idx:end_idx])
+                str_list = list(filter(None, sentence.sentence[begin_idx:end_idx]))
                 left_init, right_init = self.numericizer1.char_fofe_of_word(
-                            ''.join( [ w[0] for w in sentence.sentence[begin_idx:end_idx] ] ) )
+                            ''.join( [ w[0] for w in str_list ] ) )
                 dense_buffer[cnt,256:384] = left_init
                 dense_buffer[cnt,384:512] = right_init
 
