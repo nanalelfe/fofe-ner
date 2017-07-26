a4e062c1a36cfceb4220ba971dfe722b76c708bc
nanalelfe <nanalelfe@gmail.com>
2017-07-26 17:46:56 -0400

    new test

--- multitask-ner-trainer.py
+++ multitask-ner-trainer.py
@@ -325,17 +325,15 @@ if __name__ == '__main__':
     # Batch constructor initializes sets of processed_sentence objects, sentence1
     # (case insensitive) and sentence2 (case sensitive)
 
-        # load all KBP training data and 90% KBP test data
+    # load all KBP training data and 90% KBP test data
     source = chain( 
         imap( 
             lambda x: x[1],
             ifilter( 
                 lambda x : x[0] % 10 < 9,
-                enumerate( 
-                    imap(
+                enumerate(imap(
                         lambda x: x[:4], 
-                        LoadED( args.kbp_train_datapath
-                        ) )
+                        LoadED( args.kbp_train_datapath))
                     ) 
                 ) 
             )
