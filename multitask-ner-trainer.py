6366509bf1c69c3e8033add7cf6700a78e88f887
nanalelfe <nanalelfe@gmail.com>
2017-08-16 12:10:57 -0400

    Small fix

--- multitask-ner-trainer.py
+++ multitask-ner-trainer.py
@@ -827,7 +827,7 @@ if __name__ == '__main__':
                 f_num if config.feature_choice & (1 << 9) > 0 else 1024,
                 False, 1, 1, config.feature_choice):
 
-            c, pi, pv = mention_net.eval( example )
+            c, pi, pv = mention_net.eval( example, curr_task )
 
             cost += c * len(target(example))
             cnt += len(target(example))
