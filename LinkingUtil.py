4b1b41f629c8f5152624812c4ee57d79215a6cd3
nanalelfe <nanalelfe@gmail.com>
2017-08-22 11:47:14 -0400

    light

--- LinkingUtil.py
+++ LinkingUtil.py
@@ -145,7 +145,8 @@ def LoadED( rspecifier, language = 'eng' ):
                     continue
                 logger.info("before: " + parts[0])
                 sent, boe, eoe, target, mids, spelling = parts[0].split(u' '), [], [], [], [], []
-                logger.info("after: " + sent[0])
+                for w in sent:
+                    logger.info(w)
                 offsets = map( lambda x : (int(x[0]), int(x[1])),
                                [ offsets[1:-1].split(u',') for offsets in parts[1].split() ] )
                 assert len(offsets) == len(sent), rspecifier + '\n' + \
