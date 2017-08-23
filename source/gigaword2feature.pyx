b5629a8a949ae3305e4ca71a871a4adff419e502
nanalelfe <nanalelfe@gmail.com>
2017-08-23 14:02:56 -0400

    light

--- source/gigaword2feature.pyx
+++ source/gigaword2feature.pyx
@@ -1320,9 +1320,17 @@ class batch_constructor:
             if feature_choice & 32 > 0:
                 sentence.insert_bow( begin_idx, end_idx, cnt, bow2 )
 
+            numericizer1 = vocabulary("/eecs/research/asr/mingbin/cleaner/word2vec/gigaword/spa-gw" + '-case-insensitive.wordlist',
+                             0.8, False,
+                              n_label_type=0)
+
             cnt += 1
 
-            write_file.write(str(fragment) + " " + str(bow1[cnt]) + '\n')
+            if fragment in numericizer1.word2idx:
+                x = numericizer1.word2idx[fragment]
+            else:
+                x = 99999
+            write_file.write(str(fragment) + " " + str(x) + '\n')
 
             if cnt % n_batch_size == 0 or (i + 1) == len(candidate):
                 with nogil:
