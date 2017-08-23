0f367e508075fdabd5161c938140222d68bbcbe3
nanalelfe <nanalelfe@gmail.com>
2017-08-23 15:59:36 -0400

    light

--- source/gigaword2feature.pyx
+++ source/gigaword2feature.pyx
@@ -699,7 +699,7 @@ cdef class processed_sentence:
                 # convert the non-ascii characters to something (hexadecimal?)
                 self.sentence.push_back( u''.join( c if ord(c) < 128 else chr(ord(c) % 32) for c in list(w) ) )
                 # logger.info(w)
-                self.sentence.push_back( u''.join( c if ord(c) < 128 else str(ord(c)) for c in list(w) ) )
+                self.sentence_full.push_back( u''.join( c if ord(c) < 128 else str(ord(c)) for c in list(w) ) )
 
             vocab = numericizer
             # populate the self.numeric vector 
